e0681a45f7eff9e3da6843d2a961558f56ab5ad4
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2024-05-09 21:27:57 -0700

    Support more flexible embedding of context

--- synth/oscillator.py
+++ synth/oscillator.py
@@ -49,6 +49,7 @@ class HarmonicEmbedder(nn.Module):
         )
     def forward(self, freq, time_latent, amplitude):
         print("Shapes: ", freq.shape, time_latent.shape, amplitude.shape)
+        print("Devices: ", freq.device, time_latent.device, amplitude.device)
         return self.net(torch.cat((freq, time_latent, amplitude), dim=-1).unsqueeze(0).to(freq.device))
 
 class HarmonicEmbedderOld(nn.Module):    
